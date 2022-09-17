fee9359b9a14b043ae2e015f497d4da20e132364
Muddykat <JarrodMuddyman@gmail.com>
2022-09-17 21:08:10 +1000

    Implemented Code Skeleton for GuestUser.java

--- src/main/user/GuestUser.java
+++ src/main/user/GuestUser.java
@@ -1,4 +1,24 @@
 package main.user;
 
+import main.property.Property;
+
+import java.util.List;
+
 public class GuestUser {
+    protected int activeTime;
+    protected String sessionID;
+
+    public List<Property> findProperty(Object criteria){
+        //Implement this Method
+        return null;
+    }
+
+    public RegisteredUser registerUser(String username, String password, String email){
+        //Implement this Method
+        return null;
+    }
+
+    public void loginUser(String username, String password) {
+        //Implement this Method
+    };
 }
