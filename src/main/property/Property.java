6853569c5b7d134050736d3d79c837f2e1dbb071
Muddykat <JarrodMuddyman@gmail.com>
2022-09-17 21:23:37 +1000

    Added Skeleton Code for Property and minor changes to RegisteredUser
    Registered User now extends GuestUser

--- src/main/property/Property.java
+++ src/main/property/Property.java
@@ -1,4 +1,47 @@
 package main.property;
 
+import main.user.RealestateAgent;
+import main.user.RealestatePartner;
+
 public class Property {
+    public String name;
+    public String description;
+    private String propertyID;
+    private String location;
+    private List<LongBlob> photos;
+    private RealestateAgent agent;
+    private RealestatePartner partner;
+    public String getPropertyLocation(){
+        return null;
+    }
+
+    public void setAgent(RealestateAgent agent){
+
+    }
+
+    public void setLocation(String newLocation){
+
+    }
+
+    public boolean addPhoto(LongBlob photoData){
+
+        return false;
+    }
+
+    public boolean deltePhoto(int photoIndex){
+
+        return false;
+    }
+
+    public List<LongBlob> getPhotoListCopy(){
+        return null;
+    }
+
+    public boolean setPartner(RealestatePartner newPartner){
+        return false;
+    }
+
+    public String getID(){
+        return propertyID;
+    }
 }
--- src/main/user/RegisteredUser.java
+++ src/main/user/RegisteredUser.java
@@ -1,4 +1,4 @@
 package main.user;
 
-public class RegisteredUser {
+public class RegisteredUser extends GuestUser {
 }
