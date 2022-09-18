e72aebb8df70ef0ca412136c87599673c8c6e031
barotyuvi08 <75923908+barotyuvi08@users.noreply.github.com>
2022-09-18 13:28:46 +1000

    Update Admin.java

--- src/main/user/Admin.java
+++ src/main/user/Admin.java
@@ -1,4 +1,27 @@
 package main.user;
 
-public class Admin {
+import java.util.*
+
+public class Admin{
+    protected String username;
+    protected String email;
+    
+    public returnType 
+    
+    public boolean deleteRealestatePartner (partnerID:String, newPartnerInfo:RealestatePartner){
+        return false;
+    }
+    public boolean updateRealestatePartner(partnerID:String, newPartnerInfo:RealestastePartner){
+        return false;
+    }
+    public boolean assignAgentToProperty(agentID:String, propertyID:String){
+        return false;
+        
+    }
+    public boolean createAgent(agent:RealestateAgent){
+        return false;
+    }
+    public boolean deleteAgent(agentID:String){
+        return false;
+    }
 }
