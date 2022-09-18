e9e447fd044071add5996aa48d58ae9920518e45
barotyuvi08 <75923908+barotyuvi08@users.noreply.github.com>
2022-09-18 13:34:25 +1000

    Update PropertyApplication.java

--- src/main/property/PropertyApplication.java
+++ src/main/property/PropertyApplication.java
@@ -1,4 +1,21 @@
 package main.property;
 
-public class PropertyApplication {
+import main.property.Property;
+import main.user.RegisteredUser;
+
+import java.util.*
+
+public class PropertyApplication{
+    public ApplicationType applicationType;
+    public Boolean accepted;
+    public RegisteredUser user;
+    public Property property;
+}
+
+public boolean acceptApplication(){
+    return false;
+}
+public boolean rejectApplication(){
+    return false;
+}
 }
