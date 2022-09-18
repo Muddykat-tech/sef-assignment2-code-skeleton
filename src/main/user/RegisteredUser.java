051a57161a863a42160ad2872dc9bc829e1de013
HAOYUWANG123 <113771187+HAOYUWANG123@users.noreply.github.com>
2022-09-18 17:36:34 +1000

    Update RegisteredUser.java

--- src/main/user/RegisteredUser.java
+++ src/main/user/RegisteredUser.java
@@ -1,4 +1,23 @@
 package main.user;
 
 public class RegisteredUser extends GuestUser {
+  private String username;
+  private String email;
+  
+  pubic void logout(){
+  }
+  public void changeEmail(String oldEmail,String nweEmail){
+  }
+  public String getUsername(){
+    return username;
+  }
+  public void setUsername(String usename) {
+    this.username = username;
+  }
+  public String getEmail(){
+    return email;
+  }
+  public void setEmail(string email) {
+    this.email = email;
+  }
 }
