e0dc612a039d349cce08d9269f42e65ee1cb4a56
barotyuvi08 <75923908+barotyuvi08@users.noreply.github.com>
2022-09-18 13:36:11 +1000

    Update Enquiry.java

--- src/main/property/Enquiry.java
+++ src/main/property/Enquiry.java
@@ -1,5 +1,29 @@
 package main.property;
 
-public class Enquiry {
+import main.property.Property;
+import main.user.RegisteredUser;
+import main.user.RealestateAgent;
 
+import java.util.*
+
+public class Enquiry{
+    private EnquiryType type;
+    private String description;
+    private String SenderId;
+    private String AgentId;
+    private String enquiryId;
+    
+    public void setSenderId(String){
+        
+    }
+    public void setAgentId(String){
+        
+    }
+    public void setDescription(String){
+        
+    }
+     public void setEnquiryType(EnquiryType){
+        
+    }
+    
 }
