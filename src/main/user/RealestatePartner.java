19e7786eb66dcb6fcd69792d0af77bcaa491315b
HAOYUWANG123 <113771187+HAOYUWANG123@users.noreply.github.com>
2022-09-18 17:29:38 +1000

    Update RealestatePartner.java

--- src/main/user/RealestatePartner.java
+++ src/main/user/RealestatePartner.java
@@ -1,4 +1,43 @@
 package main.user;
 
 public class RealestatePartner {
+private String username;
+private String id;
+private List<Property> property;
+
+public Boolean registerProperty（Property newProperty）{
+    return false;
 }
+  
+  
+public Bollean deleteProperty(String propertyID){
+  return false;
+}
+public Bollean updateProperty(String propertyId,Property updated){
+  return false;
+}
+public Property getProperty(Property newProperty){
+  return new Property();
+ }
+  
+public String getUsername() {
+  return username;
+}
+  public void setUsername(String username) {
+    this.username = username;
+  }
+  public String getId(){
+    return id;
+  }
+  public void setId(String id) {
+    this.id=id;
+  }
+  public List <Property> getProperty(){
+    return property;
+  }
+  public viod setProperty(Lis<Property> property){
+    this.property = property;
+  }
+}
+      
+    
