5e0a80e84db3214cdb348b2b8527b515a4a2917a
barotyuvi08 <75923908+barotyuvi08@users.noreply.github.com>
2022-09-18 13:33:24 +1000

    Update RealestateAgent.java

--- src/main/user/RealestateAgent.java
+++ src/main/user/RealestateAgent.java
@@ -1,5 +1,21 @@
-package main.user;
+package main.property;
 
-public class RealestateAgent {
+import main.property.Property;
+import main.user.RegisteredUser;
 
+import java.util.*
+
+public class RealestateAgent{
+    public ApplicationType applicationType;
+    public Boolean accepted;
+    public RegisteredUser user;
+    public Property property;
+}
+
+public boolean acceptApplication(){
+    return false;
+}
+public boolean rejectApplication(){
+    return false;
+}
 }
